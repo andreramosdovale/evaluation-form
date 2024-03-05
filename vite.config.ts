04d63dbbf2d7717eceb82dcdc5a56a87653221fe
André Vale <andre_ramosdovale@outlook.com>
2024-03-04 21:04:33 -0300

    chore: setup code coverage

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "json", "html"],
+    },
   },
 });
