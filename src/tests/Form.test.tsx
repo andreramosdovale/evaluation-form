25b6e63c02d380eb53ac5b4877374755540efb88
André Vale <andre_ramosdovale@outlook.com>
2024-03-04 21:00:58 -0300

    refact: refatoração de caso de teste de formulario

--- src/tests/Form.test.tsx
+++ src/tests/Form.test.tsx
@@ -1,22 +1,22 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, test } from "vitest";
 import { Form } from "../components/Form";
 
 describe("Form", () => {
-  test("Should be able to see the fields on screen", () => {
-    const { getAllByPlaceholderText } = render(<Form />);
+  beforeEach(() => {
+    render(<Form />);
+  });
 
-    expect(getAllByPlaceholderText("Name"));
-    expect(getAllByPlaceholderText("Rating"));
-    expect(getAllByPlaceholderText("Comment"));
+  test("Should be able to see the fields on screen", () => {
+    expect(screen.getAllByPlaceholderText("Name"));
+    expect(screen.getAllByPlaceholderText("Rating"));
+    expect(screen.getAllByPlaceholderText("Comment"));
   });
 
   test("Should be able to see the submit button on screen", () => {
-    const { getByRole } = render(<Form />);
-
     expect(
-      getByRole("button", {
+      screen.getByRole("button", {
         name: /registrar/i,
       })
     );
